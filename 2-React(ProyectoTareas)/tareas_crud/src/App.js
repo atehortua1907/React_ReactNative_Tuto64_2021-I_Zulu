53839d77be6160a45fcc989c4597ec06efc9a65a
David Atehortua <atehortua1907@gmail.com>
2021-03-02 23:25:28 -0500

    Video 18 - Finalizar el CRUD en memoria - adición de error

--- 2-React(ProyectoTareas)/tareas_crud/src/App.js
+++ 2-React(ProyectoTareas)/tareas_crud/src/App.js
@@ -7,11 +7,21 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [editMode, setEditMode] = useState(false);
   const [id, setId] = useState("");
+  const [error, setError] = useState(null);
   
+const validForm = () =>{
+  let isValid = true;
+  setError(null);
+  if(isEmpty(task)){
+    setError("Debes ingresar una tarea.");
+    isValid = false;
+  }
+  return isValid;
+}
+
   const addTask = (e) =>{
     e.preventDefault();
-    if(isEmpty(task)){
-      console.log("Task Empty");
+    if(!validForm()){
       return;
     }
     const newTask = {
@@ -24,8 +34,7 @@ function App() {
 
   const saveTask = (e) =>{
     e.preventDefault();
-    if(isEmpty(task)){
-      console.log("Task Empty");
+    if(!validForm()){
       return;
     }
     const editedTasks = tasks.map(item => item.id == id ? {id, name:task} : item);
@@ -55,7 +64,7 @@ function App() {
           <h4 className="text-center">Lista de Tareas</h4>
           {
             size(tasks) == 0
-            ? (<h5 className="text-center">Aún no hay tareas</h5>)
+            ? (<li className="list-group-item">Aún no hay tareas</li>)
             : (
                 <ul className="list-group">
                 {
@@ -89,6 +98,9 @@ function App() {
             {editMode ? "Modificar Tarea" : "Agregar Tarea"}
           </h4>
           <form onSubmit={editMode ? saveTask : addTask}>
+            {
+              error && <span className="text-danger">{error}</span>
+            }
             <input
               type="text"
               className="form-control mb-2"
