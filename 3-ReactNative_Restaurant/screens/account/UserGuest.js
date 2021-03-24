6c93c029076674553b5a517aea4d9ba2eef5b39b
David Atehortua <atehortua1907@gmail.com>
2021-03-23 22:58:40 -0500

    Video  - 36 - Navegar a la pantalla de login

--- 3-ReactNative_Restaurant/screens/account/UserGuest.js
+++ 3-ReactNative_Restaurant/screens/account/UserGuest.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { StyleSheet, ScrollView, Text, Image } from 'react-native'
 import { Button } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 
 export default function UserGuest() {
+    const navigation = useNavigation();
     return (
         <ScrollView
             centerContent
@@ -22,7 +24,7 @@ export default function UserGuest() {
             <Button
                 buttonStyle={styles.button}
                 title="Ver tu perfil"
-                onPress={()=>{console.log("Click")}}
+                onPress={()=>{navigation.navigate("login")}}
             />
 
         </ScrollView>
@@ -37,8 +39,7 @@ const styles = StyleSheet.create({
     image: {
         height:350,
         width:"100%",
-        marginBottom: 10,
-        textAlign: "center"
+        marginBottom: 10
     },
     title: {
         fontWeight:"bold",
