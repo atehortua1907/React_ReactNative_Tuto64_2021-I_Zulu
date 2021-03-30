ac99739fafc35a5ed3e3528dbc40af1ebfeaf3df
David Atehortua <atehortua1907@gmail.com>
2021-03-29 20:42:32 -0500

    Video - 37 - Creando llamado a registrar un usuario

--- 3-ReactNative_Restaurant/screens/account/Login.js
+++ 3-ReactNative_Restaurant/screens/account/Login.js
@@ -1,12 +1,58 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Divider } from 'react-native-elements'
 
-export default function Login() {
-    return (
-        <View>
-            <Text>Login</Text>
-        </View>
+export default function Login() {    
+    return (        
+        <ScrollView>
+            <Image
+                source={require("../../assets/restaurant-logo.png")}
+                resizaMode="contain"
+                style={styles.image}
+            />
+            <View style={styles.container}>
+                <Text>Login Form</Text>
+                <CreateAccount/>
+            </View>
+            <Divider style={styles.divider}/>
+        </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+function CreateAccount(props){
+    return(
+        <Text 
+            style={styles.register}
+            onPress={()=> console.log("register")}
+        >
+            ¿Aún no tienes una cuenta?{" "}
+            <Text style={styles.btnregister}>
+                Registrate
+            </Text>
+        </Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    image:{
+        height:350,
+        width: "100%",
+        marginBottom: 20
+    },
+    container:{
+        marginHorizontal: 40
+    },
+    divider: {
+        backgroundColor: "#442484",
+        margin: 40
+    },
+    register:{
+        marginTop: 15,
+        marginHorizontal: 10,
+        alignSelf: "center"
+    },
+    btnregister:{
+        color: "#442484",
+        fontWeight: "bold"
+    }
+})
